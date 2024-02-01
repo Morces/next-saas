f1d83ceb51710393b160339b8245605d4dca3df3
Morces <karanim594@gmail.com>
2024-02-01 15:09:50 +0300

    first commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-const cn = (...inputs: ClassValue[]) => {
-    return twMerge(clsx(inputs))
+export default function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
-
-export default cn
