f71093bb9c8dff6260a71ac9cdbe49832bacdfb3
Moses Karani <karanim594@gmail.com>
2024-03-06 12:12:07 +0300

    on success issue

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,5 +1,5 @@
 import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { trpc } from "../_trpc/client";
 
 const Page = () => {
@@ -7,13 +7,13 @@ const Page = () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
-    onSuccess: ({ success }: { success: boolean }) => {
-      if (success) {
-        router.push(origin ? origin : "/dashboard");
-      }
-    },
-  });
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined);
+
+  useEffect(() => {
+    if (!isLoading && data && data.success) {
+      router.push(origin ? origin : "/dashboard");
+    }
+  }, [isLoading, data, router, origin]);
 };
 
 export default Page;
